791670f546934b32575838081f57d16bd039be4f
Cedric Renda <cedric.renda@pmodwrc.ch>
2024-07-22 17:36:57 +0200

    fix: test path to files

--- tests/unit/test_fits_class.py
+++ tests/unit/test_fits_class.py
@@ -2,15 +2,20 @@ import pytest
 from pathlib import Path
 from fits2db.fits import FitsFile
 
+DATA_DIR = Path(__file__).parent / 'data' 
+
+
+def valid_fits_path(file):
+    return DATA_DIR / file
 
 def test_valid_fits_file():
-    path = Path(r"tests\unit\data\2021-07-07_L1a.fits")
+    path = valid_fits_path("2021-07-07_L1a.fits")
     fits_file = FitsFile(path)
     assert fits_file.file_name == "2021-07-07_L1a.fits"
 
 
 def test_table_names():
-    path = Path(r"tests\unit\data\2021-07-07_L1a.fits")
+    path = valid_fits_path("2021-07-07_L1a.fits")
     fits_file = FitsFile(path)
     assert fits_file.table_names == [
         "CONTROL_NORMAL",
@@ -27,26 +32,24 @@ def test_table_names():
 
 
 def test_invalid_fits_path():
-    path = Path(r"tests\unit\data\2021_L1a.fits")
+    path = valid_fits_path("2021_L1a.fits")
     with pytest.raises(FileNotFoundError):
         FitsFile(path)
 
 
 def test_fits_path_not_a_file():
-    path = Path(r"tests\unit\data")
+    path = valid_fits_path("")
     with pytest.raises(ValueError):
         FitsFile(path)
 
 
 def test_fits_path_not_a_fits_file():
-    path = Path(r"tests\unit\data\2021-07-07_L1a.txt")
+    path = valid_fits_path("2021-07-07_L1a.txt")
     with pytest.raises(ValueError):
         FitsFile(path)
 
 
 def test_corrupt_fits_file():
-    path = Path(
-        r"tests\unit\data\2021-07-07_L1a_corrupt.fits"
-    )
+    path = valid_fits_path("2021-07-07_L1a_corrupt.fits")
     with pytest.raises(ValueError):
         FitsFile(path)
